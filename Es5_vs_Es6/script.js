6cd78e06098b99007a0d9904452e15fdb7daaa8f
meshillenus <lionjaden@gmail.com>
2024-03-05 13:34:07 +0530

    05/03/24

--- Es5_vs_Es6/script.js
+++ Es5_vs_Es6/script.js
@@ -83,3 +83,89 @@ function checkTwoObjects(obj1, obj2) {
   console.log(key2.sort());
 }
 checkTwoObjects(per1, per2);
+
+//TASK
+const men_Ar =     [1,3,5,6,["sanjay",25],9]
+
+const [ ,  ,  ,   ,[mentorname,mentorage]] =men_Ar;
+console.log(mentorname);
+console.log(mentorage);
+
+const hello ="meshil"
+const temp = `
+
+${hello}`
+
+console.log(temp)
+
+//Array destructuring...
+//["ele", "ele2"]
+const studArr = ["subash", "sanjai", ["aravind", "prahsall"]];
+console.log("--------normal structure---------");
+console.log(studArr);
+console.log(studArr[0]);
+console.log(studArr[1]);
+console.log("--------de structure---------");
+const [name1, , [name3, name4]] = studArr;
+//[name1, ,name3] = ["subash", "sanjai", ["aravind", "prahsall"]]
+//name1 = subash
+// , , = sanjai
+//[name3, name4] = ["aravind", "prahsall"]
+console.log(name1);
+console.log(name3);
+console.log(name4);
+//task
+const men_Arr = [1, 3, 5, 6, ["Sanjay", 25], 9];
+const [, , , , [menName, menAge]] = men_Arr;
+//mentorName and MentorAge
+console.log("task---------", menName, menAge);
+//Object destructuring
+const data = {
+  firstName: "sanjay",
+  lastname: "guvi",
+  pos: "mentor",
+  loc: "chennai",
+  company: {
+    loc: "chennai",
+    country: "India",
+    state: "Tamil Nandu",
+  },
+};
+console.log("....normal structure");
+console.log(data);
+console.log(data.firstName, data.lastname);
+const {
+  firstName,
+  pos,
+  lastname: lName,
+  company: { loc, state: comState, country },
+} = data;
+
+console.log(firstName); //sanjay
+console.log(pos); //mentor
+console.log(lName); // guvi (renamed)
+console.log(loc); //chennai
+console.log(comState); //tamil nadu (nested, renamed)
+console.log(country); // INDIA
+
+//property shorthand
+let batch = "FSD55WD2";
+let Time = "8.00 AM";
+
+const schedule = {
+  topic: "ES6 JS",
+  batch,
+  time: Time,
+};
+console.log(schedule);
+
+// Template literals
+const greet = "how are your";
+const yourSelf = "I am mentor in guvi";
+const totalGreetMsg = "Hi " + greet + " " + yourSelf;
+const templateStr = `Hi 
+${greet} 
+${yourSelf}
+`;
+console.log(totalGreetMsg);
+console.log(templateStr);
